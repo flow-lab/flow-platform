b1b658a45e8ced36d93e11809cd4c30186d7b4f6
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2023-05-22 21:26:44 +0200

    add: helm for postgresql

---
+++ misc/postgresql-install.sh
@@ -0,0 +1,61 @@
+NAME=postgresql
+TIMEOUT=600s
+
+DB_NAME=${DB_NAME:-diatom}
+
+# create an app user secret if it doesn't exist
+SECRET_EXISTS=$(kubectl get secrets | grep "^${NAME}-user")
+if [ -z "${SECRET_EXISTS}" ]; then
+  kubectl create secret generic ${NAME}-user \
+    --from-literal=postgres-password="$(openssl rand -hex 16)" \
+    --from-literal=PASS="$(openssl rand -hex 16)" \
+    --dry-run=client -o yaml | kubectl apply -f -
+else
+  echo "Secret ${NAME}-user already exists. Not overwriting."
+fi
+
+helm upgrade -i \
+  ${NAME} postgresql \
+  --atomic \
+  --repo https://charts.bitnami.com/bitnami \
+  --timeout ${TIMEOUT} \
+  --version 12.5.4 \
+  --values - <<EOF
+auth:
+  database: ${DB_NAME}
+  username: ${DB_NAME}
+  existingSecret: "${NAME}-user"
+  secretKeys:
+    userPasswordKey: "PASS"
+EOF
+
+# create configmap for HOST and USERNAME
+kubectl create configmap ${NAME}-config \
+  --from-literal=HOST="${NAME}.default.svc.cluster.local" \
+  --from-literal=NAME="${DB_NAME}" \
+  --from-literal=USER="${DB_NAME}" \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+# ** Please be patient while the chart is being deployed **
+  #
+  #PostgreSQL can be accessed via port 5432 on the following DNS names from within your cluster:
+  #
+  #    postgresql.default.svc.cluster.local - Read/Write connection
+  #
+  #To get the password for "postgres" run:
+  #
+  #    export POSTGRES_PASSWORD=$(kubectl get secret --namespace default postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
+  #
+  #To connect to your database run the following command:
+  #
+  #    kubectl run postgresql-client --rm --tty -i --restart='Never' --namespace default --image docker.io/bitnami/postgresql:15.3.0-debian-11-r4 --env="PGPASSWORD=$POSTGRES_PASSWORD" \
+  #      --command -- psql --host postgresql -U postgres -d postgres -p 5432
+  #
+  #    > NOTE: If you access the container using bash, make sure that you execute "/opt/bitnami/scripts/postgresql/entrypoint.sh /bin/bash" in order to avoid the error "psql: local user with ID 1001} does not exist"
+  #
+  #To connect to your database from outside the cluster execute the following commands:
+  #
+  #    kubectl port-forward --namespace default svc/postgresql 5432:5432 &
+  #    PGPASSWORD="$POSTGRES_PASSWORD" psql --host 127.0.0.1 -U postgres -d postgres -p 5432
+  #
+  #WARNING: The configured password will be ignored on new installation in case when previous Posgresql release was deleted through the helm command. In that case, old PVC will have an old password, and setting it through helm won't take effect. Deleting persistent volumes (PVs) will solve the issue.
